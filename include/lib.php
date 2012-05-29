1bba94fd00c87081ca75d4ee261a06d3468016c1
slacken <xinkiang@gmail.com>
2012-05-29 20:46:58 +0800

    sae cdn

---
+++ include/lib.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
+/**
+ * 公共函数
+ * */
+class lib{
+	
+	
+
+}
---
+++ include/view.php
@@ -0,0 +1,32 @@
+<?php
+
+if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
+
+class view{
+	
+    public function __construct(){
+        ob_start();
+    }
+    
+    public function render(){}
+    
+    public function finish(){
+        $content = ob_get_clean();
+        return $content;
+    }
+    
+    /**
+     * 直接显示
+     * */
+	public static function show($page, $data = array()){
+		$page = BASE_PATH.'views/'.trim($page,'/').'.php';
+		if(is_readable($page)){
+			ob_start();
+			
+	    	include $page;
+	    	ob_end_flush();
+		}
+	}
+	
+}
+?>
