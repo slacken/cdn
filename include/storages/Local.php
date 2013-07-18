50c88cc4478b9735c5f964f2f7343c11665cec84
oott123 <aaaatin@qq.com>
2013-07-18 18:42:43 +0800

    修复一个导致本地存储不能工作的BUG

--- include/storages/Local.php
+++ include/storages/Local.php
@@ -31,12 +31,12 @@ class StorageHandle{
 	}
 	private function get_file($key,$pre = true){
 		if(NO_KEY || NO_SECOND_FLODER){
-			$dir = $this->data_dir.$key;
+			$dir = dirname($this->data_dir.$key);
 			if(!is_dir($dir)){
 				if(!mkdir($dir,0777,true)) die(json_encode(array('error'=>'cannot_make_dir')));
 			}
 			if(!$pre) return $key;
-			return $dir;
+			return $dir.'/'.$key;
 		}
 		$letter1 = substr($key,0,1);
 		$letter2 = substr($key,0,2);
